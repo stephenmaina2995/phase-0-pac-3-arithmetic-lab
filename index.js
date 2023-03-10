2ce407c1d58ec1fe8f6c41f6d8eb68f616240f62
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-03-10 15:00:48 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+var a;
+var b;
+function add(a,b){
+    return a+b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function divide(a,b){
+    return a/b;
+}
+function multiply(a,b){
+    return a*b;
+}
+// n++
+function increment(n){
+    return ++n;
+}
+// n--
+function decrement(n){
+    return --n;
+}
+function makeInt(n=10){
+    return parseInt(n,10);
+}
+function preserveDecimal(n){
+    return parseInt(n);
+}
+function preserveDecimal(n){
+    return parseFloat(n);
+}
